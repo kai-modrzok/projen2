a87b6de05b93cb0446ae74abd238c513708e5302
kai <kai.modrzok@gmx.de>
2023-06-01 22:22:48 +0200

    feat: Add java

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,7 +9,16 @@ const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
   repositoryUrl: 'https://github.com/kai.modrzok/projen2.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    //'@aws-solutions-constructs/aws-s3-sns'
+  ],            
+  publishToMaven: {
+    mavenRepositoryUrl: 'https://maven.pkg.github.com/kai.modrzok/projen2',
+    mavenGroupId: 'kai.modrzok',
+    mavenArtifactId: 'projen2',
+    javaPackage: 'com.kai.projen2'
+  }    
+  
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket, BucketEncryption, BucketProps } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class CdkThreeTierServerlessStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const s3props: BucketProps = {
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+        bucketKeyEnabled: true,
+        encryption: BucketEncryption.KMS,
+        enforceSSL: true,
+    }
+    new Bucket(this, 'S3Bucket', s3props);
   }
 }
